4166d885e440b6fc5d646edb18e8055ccc80a86b
ShadyAgami <sagamy@road9media.com>
2020-06-15 03:38:28 +0200

    cv

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users", {mode: 'cors'})
+    fetch("https://jsonplaceholder.typicode.com/users", {mode: 'no-cors'})
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
     
