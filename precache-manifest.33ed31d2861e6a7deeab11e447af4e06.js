3dd5766ab37b69d26a2ab866fd9dcc3b0fa8c30c
ShadyAgami <sagamy@road9media.com>
2020-06-15 03:26:01 +0200

    Updates

---
+++ precache-manifest.33ed31d2861e6a7deeab11e447af4e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd2788cef6ac24754f3680627b92d91",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6114838b627eaf3eb7ef",
+    "url": "/Monsters-rolodex/static/css/main.d624bf7c.chunk.css"
+  },
+  {
+    "revision": "ce7c83e346e5fbe102d3",
+    "url": "/Monsters-rolodex/static/js/2.aac49012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.aac49012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6114838b627eaf3eb7ef",
+    "url": "/Monsters-rolodex/static/js/main.617486cf.chunk.js"
+  },
+  {
+    "revision": "34b54faee5f29a8cbe67",
+    "url": "/Monsters-rolodex/static/js/runtime-main.d63307db.js"
+  }
+]);
